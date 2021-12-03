eb57a5e99670c9bf336b557b62cfa4c274d2fd78
Alex Dixon <alex@dixonary.co.uk>
2021-12-03 12:16:18 +0000

    Day 3

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -9,31 +9,63 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
+import Data.Char
 import Data.Void
+import Data.Ord (comparing)
+
+import Debug.Trace
+
+import {- # EXTREMELY_CURSED # -} Control.Arrow ((&&&), (>>>))
+import Data.Bifunctor
 {- ORMOLU_ENABLE -}
+
+import Data.Foldable
+import Data.Function ((&))
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 (read.pure <$> digit) `sepBy` skipSpace
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
-
-type OutputB = Void
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = uncurry (*) 
+      . (fromBinary &&& fromBinary . map (1-)) 
+      . map (head.modes) 
+      . transpose
+
+modes :: [Int] -> [Int]
+modes = freq >>> \f -> case compare (f Map.! 0) (f Map.! 1) of
+  LT -> [1]
+  GT -> [0]
+  EQ -> [0,1]
+
+fromBinary :: [Int] -> Int
+fromBinary = foldl1' (\a x -> a*2+x)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB xs = oxygenRating * co2Rating
+  where
+    oxygenRating = getRating
+                 $ \case {[m] -> (== m); [0,1] -> (== 1)}
+                 
+    co2Rating    = getRating
+                 $ \case {[m] -> (/= m); [0,1] -> (== 0)}
+
+    getRating crit = fromBinary $ head $ head $ filter ((== 1).length) 
+                   $ scanl' getRating' xs [0..length (head xs) - 1]
+      where
+        getRating' rem i = 
+          let mi = map modes (transpose rem) !! i
+          in filter (crit mi . (!!i)) rem
