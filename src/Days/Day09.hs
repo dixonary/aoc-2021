2231fedfdd3373ff00d29db2ed64abf75f362084
Alex Dixon <alex@dixonary.co.uk>
2021-12-09 12:54:29 +0000

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -9,11 +9,13 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util as U
+import Util.Parsers as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (take)
 import Data.Void
+import Debug.Trace
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +23,32 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser (pure.read.pure) 0
 
 ------------ TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+type Input = Map (Int,Int) Int
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA m = sum $ (+1) <$> lowPoints m
+
+lowPoints :: Map (Int,Int) Int -> Map (Int,Int) Int
+lowPoints m = Map.filterWithKey isLowPoint m
+  where isLowPoint (x,y) k = all (k <) $ mapMaybe (m Map.!?)
+          [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB m = product $ take 3 $ sortOn negate $ map length getBasins
+  where
+    m' = Map.filter (/= 9) m
+    getBasins = map basin $ Map.keys $ lowPoints m'
+    basin (u,v) = converge expand $ Set.singleton (u,v)
+    
+    expand cs = cs <> Set.fromList 
+              [c' 
+              | c@(x,y) <- Set.toList cs
+              , c' <- [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
+              , c' `Map.member` m'
+              , m Map.! c < m Map.! c'
+              ]
