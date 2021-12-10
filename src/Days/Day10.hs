47b7a7d3cdd9e37e7bd3fa88318c053915d5d383
Alex Dixon <alex@dixonary.co.uk>
2021-12-10 13:17:32 +0000

    Day 10

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -14,6 +14,7 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Char (isSpace)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,49 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = map (\x -> tryParse (x, [])) 
+              <$> many1 (satisfy (not.isSpace)) `sepBy` space
 
 ------------ TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+type Input = [PartialParse]
+type PartialParse = (String,String)
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = sum . map (score.head.fst) . filter (not.isIncomplete) 
+
+tryParse :: (String, String) -> (String, String)
+tryParse ([], s) = ([], s)
+tryParse (x:xs, s)
+  | isOpen x = tryParse (xs, x:s)
+  | otherwise = case s of
+      [] -> (x:xs, [])
+      s:ss | s == paired x -> tryParse (xs, ss)
+      _ -> (x:xs, s)
+
+isIncomplete :: PartialParse -> Bool
+isIncomplete = null.fst
+
+isOpen :: Char -> Bool
+isOpen = (`elem` ("{[<("::String))
+
+paired :: Char -> Char
+paired = (Map.fromList (zip "}]>)" "{[<(") Map.!)
+
+score :: Char -> Int
+score ')' = 3
+score ']' = 57
+score '}' = 1197
+score '>' = 25137
+score '(' = 1
+score '[' = 2
+score '{' = 3
+score '<' = 4
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = median
+        . map (foldl1' (\a x -> a*5+x) . map score .snd)
+        . filter isIncomplete
+  where 
+    median xs = sort xs !! (length xs `div` 2)
