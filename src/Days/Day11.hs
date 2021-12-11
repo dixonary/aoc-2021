8713a901ea7eda79020a72217b59ef39cd316d83
Alex Dixon <alex@dixonary.co.uk>
2021-12-11 12:29:53 +0000

    Day 11

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -9,11 +9,15 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (take, takeWhile)
 import Data.Void
+import Util.Parsers (coordinateParser)
+import Data.Function
+import Debug.Trace
+import Control.Arrow
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +25,58 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser (pure . Normal . read . pure) 0
 
 ------------ TYPES ------------
-type Input = Void
+data Octo = Normal Int | Flashed deriving (Eq)
+instance Show Octo where 
+  show = \case 
+    Normal x | x > 9 -> "X"; 
+    Normal x         -> show x;
+    Flashed -> "F"
 
-type OutputA = Void
+type Octos = Map (Int,Int) Octo
 
-type OutputB = Void
+type Input = Octos
+
+instance {-# OVERLAPS #-} Show Octos where
+  show os = unlines 
+          $ map (concatMap (show . snd))
+          $ groupBy ((==) `on` (snd.fst)) $ sortOn (snd.fst) 
+          $ Map.assocs os
+
+grow :: Octo -> Octo
+grow (Normal x) = Normal (x+1)
+grow Flashed    = Flashed
+
+isReady :: Octo -> Bool
+isReady = \case
+  Normal x -> x > 9
+  Flashed  -> False
+  
+reset :: Octo -> Octo
+reset = \case 
+  Normal x -> Normal x
+  Flashed  -> Normal 0   
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = snd . fpow step 100 . (,0)
+
+step :: (Octos, Int) -> (Octos, Int)
+step = first (fmap reset) . flash . first (fmap grow)
+
+flash :: (Octos, Int) -> (Octos, Int)
+flash (os, fs) = case Map.keys $ Map.filter isReady os of
+  []        -> (os,fs)
+  ((x,y):_) -> flash                    -- Go again
+    $ (,fs+1)                           -- Increment flash counter
+    $ Map.insert (x,y) Flashed          -- Mark (x,y) as Flashed
+    $ foldr (Map.adjust grow) os        -- Grow all
+    $ (,) <$> [x-1..x+1] <*> [y-1..y+1] -- Adjacents (includes self)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = length 
+      . takeWhile (\(os,_) -> not $ all (== os Map.! (0,0)) os) 
+      . iterate step . (,0)
