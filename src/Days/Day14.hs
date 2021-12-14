67c2ea927fc681a65737d5fea5005730f8f3e4d3
Alex Dixon <alex@dixonary.co.uk>
2021-12-14 13:11:53 +0000

    Day 14

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -9,11 +9,19 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Parsers as U
+import Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (sepBy)
+import Control.Applicative.Combinators
 import Data.Void
+import Data.Char (isUpper)
+import qualified Data.Text as Text
+import Control.Applicative
+import Control.Arrow
+import Data.Function
+import Data.Functor
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +29,42 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  polymer <- freq . (\x -> zip x $ tail x) . Text.unpack <$> takeWhile1 isUpper
+  skipSpace
+  rules <- liftA2 (,) (liftA2 (,) letter letter) (" -> " *> letter)
+           `sepBy` skipSpace
+  pure (polymer, Map.fromList rules)
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Polymer, Rules)
 
-type OutputA = Void
+type Polymer = Map (Char, Char) Int
+type Rules   = Map (Char, Char) Char
 
-type OutputB = Void
+step :: Rules -> Polymer -> Polymer
+step r p = Map.fromListWith (+) $ Map.assocs p >>= insert
+  where
+  insert ((a,b),count) = map (,count) 
+    $ case r Map.!? (a,b) of { Nothing -> [(a,b)]; Just c -> [(a,c),(c,b)] }
+
+compute :: Int -> Polymer -> Rules -> Int
+compute n p r = getCount $ step r `fpow` n $ p
+  where
+  -- This function takes the max total occurrences of every letter
+  -- in first OR second position (to cover the final letter)
+  -- then gets the max and min occurrences of that and takes the difference.
+  getCount p = uncurry (-)
+             $ (maximum &&& minimum)
+             $ uncurry (Map.unionWith max)
+             $ ((&&&) `on` Map.mapKeysWith (+)) fst snd p
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+-- partA :: Input -> 
+partA :: Input -> Int
+partA = uncurry $ compute 10
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = uncurry $ compute 40
