bf55665206dd455c4f050dfd97385a7f7b7ade03
Alex Dixon <alex@dixonary.co.uk>
2021-12-18 14:29:47 +0000

    Day 17

--- src/Days/Day17.hs
+++ src/Days/Day17.hs
@@ -9,11 +9,13 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util as U
+import Util.Parsers as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (takeWhile, take)
 import Data.Void
+import Debug.Trace
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +23,34 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = "target area: x=" *> signed decimal `around` ".." `around` ", y="
 
 ------------ TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+type Input = ((Int, Int), (Int, Int))
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = maximum . getHeights
+
+getHeights :: Input -> [Int]
+getHeights i@((_,r), (b,_))
+  = mapMaybe (doTrace i 0 . steps)
+  $ (,) <$> (negate (abs r) ... abs r) <*> (negate (abs b) ... abs b)
+
+doTrace :: Input -> Int -> [(Int, Int)] -> Maybe Int
+doTrace ((l,r), (b,t)) = doTrace'
+  where
+    doTrace' highest ((x,y):rest)
+      | x >  r || y <  b = Nothing
+      | x >= l && y <= t = Just highest
+      | otherwise        = doTrace' (max highest y) rest
+
+velStep :: (Ord a, Num a, Num b) => (a, b) -> (a, b)
+velStep (vx,vy) = (signum vx * max 0 (abs vx - 1), vy - 1)
+
+steps :: (Int, Int) -> [(Int, Int)]
+steps = scanl' (\(x,y) (vx,vy) -> (x+vx, y+vy)) (0,0) . iterate velStep
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = length . getHeights
