b2c78e34c92ea73fafb9ffb02c8efa2470ddb0a2
Alex Dixon <alex@dixonary.co.uk>
2021-12-18 01:13:46 +0000

    Day 16

--- src/Days/Day16.hs
+++ src/Days/Day16.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Days.Day16 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -10,10 +11,22 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Util.Parsers as P (parseErr)
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text as Parser
 import Data.Void
+import Numeric (readHex)
+import Text.Printf
+import Debug.Trace
+
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Control.Applicative (Alternative(many, (<|>)))
+import Control.Monad (liftM)
+import Data.Functor
+import Data.Either (fromRight)
+import Data.Function (fix, (&))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +34,76 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parseErr packetP
+            . Text.pack 
+            . concatMap (printf "%04b" . fst . head . readHex @Int . pure)
+            . Text.unpack
+            <$> takeText
+
+packetP :: Parser VersionedPacket
+packetP = do
+  v <- binary 3
+  VersionedPacket v <$> do
+    t <- binary 3
+    case t of
+      4 -> Literal <$> do
+        let readLit x = do
+              keepReading <- toEnum     <$> binary 1
+              valSoFar    <- (x * 16 +) <$> binary 4
+              (if keepReading then readLit else pure) valSoFar
+        readLit 0
+      o -> do
+        lt <- binary 1
+        case lt of
+          0 -> do
+            len <- binary 15
+            Operator o . parseErr (many packetP)
+              <$> Parser.take len
+          1 -> do
+            len <- binary 11
+            Operator o
+              <$> count len packetP
+
+binary :: Int -> Parser Int
+binary n = foldl1' (\r x -> r*2+x) <$> count n (("0" $> 0) <|> ("1" $> 1))
 
 ------------ TYPES ------------
-type Input = Void
+type Input = VersionedPacket
 
-type OutputA = Void
+data VersionedPacket = VersionedPacket
+   { version :: Int 
+   , packet  :: Packet 
+   }
+  deriving Show
 
-type OutputB = Void
+data Packet 
+  = Literal Int 
+  | Operator Int [VersionedPacket] 
+  deriving Show
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = versionSum
+
+versionSum :: VersionedPacket -> Int
+versionSum (VersionedPacket v p) = v + case p of
+  Literal  _    -> 0
+  Operator _ ps -> sum $ map versionSum ps
+
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = eval
+
+eval :: VersionedPacket -> Int
+eval (VersionedPacket v p) = eval' p
+  where
+    eval' (Literal n)     = n
+    eval' (Operator o ps) = fmap eval ps & case o of
+      0 -> sum 
+      1 -> product
+      2 -> minimum
+      3 -> maximum
+      5 -> \(x:y:_) -> fromEnum $ x > y
+      6 -> \(x:y:_) -> fromEnum $ x < y
+      7 -> \(x:y:_) -> fromEnum $ x == y
