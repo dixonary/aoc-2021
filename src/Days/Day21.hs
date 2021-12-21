6340ccd9f389ff9f7b806f08f1c49632efb745d7
Alex Dixon <alex@dixonary.co.uk>
2021-12-21 15:31:01 +0000

    Day 21

--- src/Days/Day21.hs
+++ src/Days/Day21.hs
@@ -9,11 +9,16 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Monad (replicateM)
+import Data.Biapplicative
+import Control.Applicative
+import Data.STRef
+import Control.Monad.ST
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +26,66 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (,)
+  <$> ("Player 1 starting position: " *> decimal <* skipSpace)
+  <*> ("Player 2 starting position: " *> decimal)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Int,Int)
 
-type OutputA = Void
+data Turn = One | Two deriving (Eq, Ord)
+type TurnState = (Turn, Int, [Int], [Int], [Int], Int, Int)
 
-type OutputB = Void
+detDie :: [Int]
+detDie = cycle [1..100]
+
+diracDist :: [(Int, Integer)]
+diracDist = [(3,1), (4, 3), (5,6), (6,7), (7,6), (8,3), (9,1)]
+
+places :: [Int]
+places = cycle [1..10]
+
+turn :: TurnState -> TurnState
+turn (t, n, a:b:c:d, p1, p2, s1, s2) = case t of
+  One -> let p1'@(p:_) = drop (a+b+c) p1 in (Two, n+3, d, p1', p2, s1+p, s2)
+  Two -> let p2'@(p:_) = drop (a+b+c) p2 in (One, n+3, d, p1, p2', s1, s2+p)
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA (i1, i2) =
+  (\(_,n,_,_,_,s1,s2) -> n * min s1 s2)
+  $ head
+  $ dropWhile (\(_,_,_,_,_,s1,s2) -> s1 < 1000 && s2 < 1000)
+  $ iterate turn
+    ( One, 0, detDie
+    , drop (i1-1) places
+    , drop (i2-1) places
+    , 0, 0)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Integer
+partB (a,b) = uncurry max $ victories (One, a, b, 21, 21)
+
+victories :: (Turn, Int, Int, Int, Int) -> (Integer, Integer)
+victories (t, p1, p2, s1, s2) = runST $ do
+  m <- newSTRef (Map.empty :: Map (Turn, Int, Int, Int, Int) (Integer, Integer))
+  let 
+    v (t, p1, p2, s1, s2)
+      | t == Two && s1 <= 0 = pure (1,0)
+      | t == One && s2 <= 0 = pure (0,1)
+      | otherwise = do
+          mm <- readSTRef m
+          case Map.lookup (t, p1, p2, s1, s2) mm of
+            Just x -> pure x
+            Nothing -> do
+              (w1,w2) <- fmap sum2 $ sequence $ do
+                (roll, count) <- diracDist
+                let
+                  addRoll p = (p + roll - 1) `mod` 10 + 1
+                  m' = case t of
+                    One -> let p' = addRoll p1 in (Two, p', p2, s1 - p', s2)
+                    Two -> let p' = addRoll p2 in (One, p1, p', s1, s2 - p')
+                pure $ both (*count) <$> v m'
+              modifySTRef m (Map.insert (t, p1, p2, s1, s2) (w1,w2))
+              pure (w1, w2)
+  v (t, p1, p2, s1, s2)
